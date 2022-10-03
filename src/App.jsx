34e4bd51f6e4c31510e05d11d073ae152c5735f0
sanikumar <sanikumarsahani@gmail.com>
2022-10-03 06:57:32 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginDialog from './components/login/login-dialog'
+
+function App() {
+
+  return (
+    <div className="">
+      <LoginDialog/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/login/login-dialog.jsx
@@ -0,0 +1,9 @@
+const LoginDialog = () => {
+  return (
+    <div>
+      <h1>Login Dialog</h1>
+    </div>
+  )
+}
+
+export default LoginDialog
